4b260c799bbc3132e7432f42bc046686ed190a0e
Marcin Matłacz <marcin@matlacz.eu>
2017-03-05 07:06:53 +0100

    Return prediction

--- hackupc2017.py
+++ hackupc2017.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, request, redirect, url_for
+from flask import Flask, request, redirect
 from flask import render_template
 from werkzeug.utils import secure_filename
 
@@ -12,24 +12,25 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/')
 def hello_world():
-	return 'Hello World!'
+    return render_template('../index.html')
 
 
-@app.route('/photo/',  methods=['GET', 'POST'])
+@app.route('/photo/', methods=['GET', 'POST'])
 def upload_photo():
-	if request.method == 'GET':
-		return render_template('file_upload_test.html')
-	if request.method == 'POST':
-		print(request.files)
-		if 'photo' not in request.files:
-			print('No photo in request')
-			return redirect(request.url)
-		file = request.files['photo']
-		if file:
-			filename = secure_filename(file.filename)
-			file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-			return render_template('file_upload_test.html', message="You have successfully uploaded a photo.")
+    if request.method == 'GET':
+        return render_template('index.html')
+    if request.method == 'POST':
+        print(request.files)
+        if 'photo' not in request.files:
+            print('No photo in request')
+            return redirect(request.url)
+        file = request.files['photo']
+        if file:
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return render_template('index.html',
+                                   message="You have successfully uploaded a photo.")
 
 
 if __name__ == '__main__':
-	app.run()
+    app.run()
