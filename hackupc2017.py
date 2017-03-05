e80a338f88677cb432e0940135d1614d9bd39e6b
Paweł <pawel.mucha@yahoo.pl>
2017-03-05 04:36:50 +0100

    file uploading done

--- hackupc2017.py
+++ hackupc2017.py
@@ -4,7 +4,7 @@ from flask import Flask, request, redirect, url_for
 from flask import render_template
 from werkzeug.utils import secure_filename
 
-UPLOAD_FOLDER = '/uploads'
+UPLOAD_FOLDER = '/tmp/photos'  # TODO: create this folder on production and chmod
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -20,6 +20,7 @@ def upload_photo():
 	if request.method == 'GET':
 		return render_template('file_upload_test.html')
 	if request.method == 'POST':
+		print(request.files)
 		if 'photo' not in request.files:
 			print('No photo in request')
 			return redirect(request.url)
@@ -27,7 +28,7 @@ def upload_photo():
 		if file:
 			filename = secure_filename(file.filename)
 			file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-			return redirect(url_for('uploaded_file', filename=filename))
+			return render_template('file_upload_test.html', message="You have successfully uploaded a photo.")
 
 
 if __name__ == '__main__':
